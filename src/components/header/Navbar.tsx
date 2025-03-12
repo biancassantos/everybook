6128a0f4740a259adfee62a6c1a5b9b7903b3ddb
Bianca Santos <biancassantos89@gmail.com>
2025-03-12 20:04:13 -0300

    feat: navbar links icons and text modified

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,15 +1,17 @@
 import NavbarLink from "./NavbarLink";
 import { GiBookshelf } from "react-icons/gi";
-import { FaBookOpen, FaBookmark, FaStar } from "react-icons/fa";
+import { GrInProgress } from "react-icons/gr";
+import { FaBookmark } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
 
 function Navbar() {
   return (
     <nav className="self-start">
       <ul className="flex gap-5 justify-between">
         <NavbarLink text="My bookshelf" url="/" icon={<GiBookshelf />} />
-        <NavbarLink text="Reading now" url="/reading" icon={<FaBookOpen />} />
-        <NavbarLink text="Next reads" url="/next-reads" icon={<FaBookmark />} />
-        <NavbarLink text="Favorites" url="/favorites" icon={<FaStar />} />
+        <NavbarLink text="Reading now" url="/reading" icon={<GrInProgress />} />
+        <NavbarLink text="Next readings" url="/next-readings" icon={<FaBookmark />} />
+        <NavbarLink text="Favorites" url="/favorites" icon={<MdFavorite className="text-lg" />} />
       </ul>
     </nav>
   )
