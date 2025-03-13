2f94314207360878613ceefb3c81de425af4129f
Bianca Santos <biancassantos89@gmail.com>
2025-03-13 19:35:57 -0300

    feat: user menu functionality added

--- src/components/header/UserArea.tsx
+++ src/components/header/UserArea.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import UserMenu from "./UserMenu";
 
 function UserArea() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <button className="text-primary text-4xl cursor-pointer relative">
+      <button
+      onClick={() => setIsOpen(!isOpen)}
+      className="text-primary text-4xl cursor-pointer relative hover:text-primarydark duration-300"
+      >
         <FaUserCircle />
       </button>
-      <UserMenu />
+      <UserMenu isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   )
 }
--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -1,19 +1,33 @@
 import { Link } from "react-router";
+import classNames from "classnames";
 
-function UserMenu() {
+type UserMenuProps = {
+  isOpen: boolean,
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function UserMenu({ isOpen, setIsOpen }: UserMenuProps) {
+  const navClass = classNames("absolute right-12 top-19 z-100 shadow-lg invisible",
+    {"visible": isOpen}
+  )
+
   return (
-    <nav className="absolute right-12 top-19 z-100 shadow-lg">
-      <ul className="bg-latte w-[200px] text-primarydark  hover:text-primary duration-300 rounded-md rounded-tr-none">
-        <li className="p-3 border-b border-b-primary text-center active:scale-90 duration-300">
+    <nav className={navClass}>
+      <ul className="bg-latte w-[200px] text-primarydark hover:text-primary transition-colors duration-300 rounded-md rounded-tr-none">
+        <li className="p-3 border-b border-b-primary text-center active:scale-90 transition-transform duration-300">
           <Link 
           to="/settings"
           className="block"
+          onClick={() => setIsOpen(false)}
           >
             Settings
           </Link>
         </li>
         <li className="p-3 text-center">
-          <button className="font-semibold w-full cursor-pointer active:scale-90 duration-300">
+          <button 
+          onClick={() => setIsOpen(false)}
+          className="font-semibold w-full cursor-pointer active:scale-90 transition-transform duration-300"
+          >
             Logout
           </button>
         </li>
