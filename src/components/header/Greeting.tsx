c0cbd01ae82c5bfe80a031b32c74dc242182067e
Bianca Santos <biancassantos89@gmail.com>
2025-03-22 11:55:59 -0300

    fix: time of the day logic fixed

--- src/components/header/Greeting.tsx
+++ src/components/header/Greeting.tsx
@@ -12,14 +12,14 @@ function Greeting() {
   const hours = date.getHours();
 
   useEffect(() => {
-    if (hours >= 12 && hours < 17) {
+    if (hours >= 4 && hours < 12) {
+      setTimeOfDay("morning");
+    } else if (hours >= 12 && hours < 17) {
       setTimeOfDay("afternoon");
     } else if (hours >= 17 && hours < 20) {
       setTimeOfDay("evening");
-    } else if (hours >= 20) {
-      setTimeOfDay("night");
     } else {
-      setTimeOfDay("morning");
+      setTimeOfDay("night");
     }
   }, [hours])
 
