93baa64a25636f0f4161613a1bb840cc674290c2
Bianca Santos <biancassantos89@gmail.com>
2025-03-24 16:10:46 -0300

    feat: disabled state design added

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -5,15 +5,16 @@ type ActionButtonProps = React.ComponentPropsWithoutRef<"button"> & {
   children: React.ReactNode
 }
 
-function ActionButton({ onClick, isFilled, children }: ActionButtonProps) {
+function ActionButton({ onClick, disabled=false, isFilled, children }: ActionButtonProps) {
   const buttonClass = classNames(
-    "flex items-center gap-3 border border-primarydark rounded-lg py-1 px-5 font-semibold hover:border-transparent duration-300 cursor-pointer",
+    "flex items-center gap-3 border border-primarydark rounded-lg py-1 px-5 font-semibold hover:border-transparent duration-300 cursor-pointer disabled:opacity-80 disabled:cursor-default",
     {"bg-primary text-latte border-primary! hover:border-primary!": isFilled}
   )
 
   return (
     <button 
     onClick={onClick}
+    disabled={disabled}
     className={buttonClass}
     >
       {children}
