e170392dbb1adba9888f009caf51b2f310fcaa5b
Bianca Santos <biancassantos89@gmail.com>
2025-03-25 20:43:33 -0300

    feat: books context created

---
+++ src/contexts/BooksContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { UserContext } from "./UserContext";
+import { db } from "../services/firebase-config";
+import { collection } from "firebase/firestore";
+import { onSnapshot } from "firebase/firestore";
+import type { NodeChildrenProps, BooksCollection, UserBook } from "../@types";
+
+type ContextValue = {
+  booksCollectionRef: BooksCollection,
+  allBooks: UserBook[]
+}
+
+export const BooksContext = createContext<ContextValue | null>(null);
+
+export function BooksContextProvider({ children }: NodeChildrenProps) {
+  const [allBooks, setAllBooks] = useState<UserBook[] | []>([]);
+  const booksCollectionRef = collection(db, "books");
+
+  const currentUser = useContext(UserContext);
+  const uid = currentUser?.user?.uid;
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(booksCollectionRef, snapshot => {
+      const list: unknown[] = [];
+
+      snapshot.docs.forEach(doc => {
+        if (doc.data().uid === uid) {
+          list.push({...doc.data(), id: doc.id});
+        }
+      });
+
+      setAllBooks(list as UserBook[]);
+    })
+
+    return () => unsubscribe();
+  }, [booksCollectionRef, uid])
+
+  const value: ContextValue = { booksCollectionRef, allBooks }
+
+  return <BooksContext.Provider value={value}>
+    {children}
+  </BooksContext.Provider>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { UserContextProvider } from './contexts/UserContext.tsx'
+import { BooksContextProvider } from './contexts/BooksContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserContextProvider>
-      <App />
+      <BooksContextProvider>
+        <App />
+      </BooksContextProvider>
     </UserContextProvider>
   </StrictMode>,
 )
