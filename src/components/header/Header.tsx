00a1dc8e12fc038a49dd37a57b84a62e2a187586
Bianca Santos <biancassantos89@gmail.com>
2025-03-26 18:52:12 -0300

    feat: user menu arrow detail added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import Searchbar from "./Searchbar";
 function Header() {
   return (
     <header className="flex flex-col gap-8 py-8 px-5 max-w-[1580px] mx-auto sm:gap-6 sm:px-10">
-      <div className="flex justify-between items-center gap-4 mb-6">
+      <div className="flex justify-between items-start gap-4 mb-6 xs:items-center">
         <Greeting />
         <UserArea />
       </div>
--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -16,11 +16,11 @@ function UserMenu({ isOpen, setIsOpen }: UserMenuProps) {
     navigate("/login");
   }
 
-  const navClass = classNames("absolute right-7 top-20 z-100 shadow-lg invisible xs:top-19 sm:right-12", {"visible": isOpen})
+  const navClass = classNames("absolute right-6 top-21 z-100 shadow-lg invisible sm:right-12", {"visible": isOpen})
 
   return (
     <nav className={navClass}>
-      <ul className="bg-latte w-[180px] text-primarydark hover:text-primary transition-colors duration-300 rounded-md rounded-tr-none xs:w-[200px]">
+      <ul className="bg-latte w-[150px] text-primarydark hover:text-primary transition-colors duration-300 rounded-md rounded-tr-none before:size-4 before:bg-latte before:absolute before:right-1 before:top-[-7px] before:rotate-45">
         <li className="p-3 border-b border-b-primary text-center active:scale-90 transition-transform duration-300">
           <Link 
           to="/settings"
