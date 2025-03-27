5f0e1e144a52bd549f738f9a19d6b40dd497a8d6
Bianca Santos <biancassantos89@gmail.com>
2025-03-27 20:03:18 -0300

    feat: searching message created and added

---
+++ src/components/SearchingMessage.tsx
@@ -0,0 +1,11 @@
+function SearchingMessage() {
+  return (
+    <div className="mx-auto my-8 px-5 py-8 text-center">
+      <p className="text-primarydark animate-pulse">
+        Searching...
+      </p>
+    </div>
+  )
+}
+
+export default SearchingMessage;
--- src/components/header/SearchList.tsx
+++ src/components/header/SearchList.tsx
@@ -1,4 +1,5 @@
 import SearchListItem from "./SearchListItem";
+import SearchingMessage from "../SearchingMessage";
 import type { Book } from "../../@types";
 
 type SearchListProps = {
@@ -7,20 +8,25 @@ type SearchListProps = {
 }
 
 function SearchList({ books, clearSearch }: SearchListProps) {
+  const booksPending = books.length == 0;
+
   return (
     <section className="absolute top-10.5 right-4 max-w-[210px] max-h-[300px] bg-latte shadow-lg overflow-y-scroll z-index-90 sm:max-w-[260px] sm:w-[260px]">
-      <ul>
-        {books.map((book: Book) => {
-          return <SearchListItem 
-          key={book.cover_edition_key}
-          bookTitle={book.title}
-          author={book.author_name}
-          coverUrl={`https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-S.jpg`}
-          bookKey={book.key}
-          clearSearch={clearSearch}
-          />
-        })}
-      </ul>
+      {booksPending ? 
+        <SearchingMessage /> :
+        <ul>
+          {books.map((book: Book) => {
+            return <SearchListItem 
+            key={book.cover_edition_key}
+            bookTitle={book.title}
+            author={book.author_name}
+            coverUrl={`https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-S.jpg`}
+            bookKey={book.key}
+            clearSearch={clearSearch}
+            />
+          })}
+        </ul>
+      }
     </section>
   )
 }
