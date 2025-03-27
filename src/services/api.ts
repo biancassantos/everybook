329c4ed762fe889e16c8a99a80379cbc5c408e01
Bianca Santos <biancassantos89@gmail.com>
2025-03-27 17:11:41 -0300

    refactor: changed to arrow functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import type { Book } from "../@types";
 
-async function getBooks(bookName: string): Promise<Book[]> {
+const getBooks = async (bookName: string): Promise<Book[]> => {
   const request = await axios.get(`https://openlibrary.org/search.json?q=${bookName}&limit=12`);
   return request.data.docs;
 }
 
-async function getBook(bookName: string, key: string): Promise<Book> {
+const getBook = async (bookName: string, key: string): Promise<Book> => {
   const request = await axios.get(`https://openlibrary.org/search.json?q=${bookName}/works/${key}`);
   return request.data.docs[0];
 }
