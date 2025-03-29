84288ad19d81ce8f3ade307db01871270a8562a3
Bianca Santos <biancassantos89@gmail.com>
2025-03-29 14:05:58 -0300

    feat: font size changed

--- src/pages/settings/components/SectionHeader.tsx
+++ src/pages/settings/components/SectionHeader.tsx
@@ -1,7 +1,7 @@
 function SectionHeader({ children }: {children: string}) {
   return (
     <div className="flex items-center gap-4 mb-5">
-      <h2 className="text-xl text-primary font-semibold  xs:text-2xl">
+      <h2 className="text-lg text-primary font-semibold xs:text-xl">
         {children}
       </h2>
       <hr className="border border-secondary w-full" />
