30eacdc46af620c5e758a92a885e31a781eff394
Bianca Santos <biancassantos89@gmail.com>
2025-03-30 19:28:24 -0300

    feat: error message modified and input reset added

--- src/pages/settings/components/ChangePasswordForm.tsx
+++ src/pages/settings/components/ChangePasswordForm.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import classNames from "classnames";
 import ActionButton from "../../../components/ActionButton";
 import { changeUserPassword } from "../../../services/firebase";
-import { getErrorMessage } from "../../../utils/helpers";
 import { toast } from "sonner";
 
 const changePasswordSchema = z.object({
@@ -18,13 +16,12 @@ function ChangePasswordForm() {
       register,
       watch,
       handleSubmit,
+      reset,
       formState: {
         errors,
         isSubmitting
       }
     } = useForm({resolver: zodResolver(changePasswordSchema)});
-
-  const [generalError, setGeneralError] = useState(""); /* Mainly Firebase errors */
 
   const password = watch("password");
   const newPassword = watch("newPassword");
@@ -37,10 +34,10 @@ function ChangePasswordForm() {
     try {
       await changeUserPassword(password, newPassword);
       toast.success("Password changed successfully!");
+      reset();
     } catch (error) {
-      const message = getErrorMessage(error);
-      if (message) setGeneralError(message);
-      toast.error("Failed to change password");
+      console.error(error);
+      toast.error("Failed to change password, check your credentials and try again");
     }
   }
 
@@ -79,8 +76,6 @@ function ChangePasswordForm() {
         className={classNames(inputClass, {"border-red-500": errors.newPassword})}
         />
         {errors.newPassword && <span className={errorClass}>{errors.newPassword.message}</span>}
-
-        {generalError && <span className={errorClass}>{generalError}</span>}
       </section>
       
       <ActionButton
